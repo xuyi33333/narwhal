1d0a5cdc5804a226911a54929aa208699ba0af21
xubangbang <972112732@qq.com>
2021-11-15 13:20:20 +0800

    .

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -19,6 +19,7 @@ def local(ctx, debug=True):
         'rate': 50_000,
         'tx_size': 512,
         'duration': 20,
+        'clients': 4,
     }
     node_params = {
         'header_size': 1_000,  # bytes
