d56031bed3c66099baab2fb983df0eda1576c43a
xubangbang <972112732@qq.com>
2021-11-20 22:14:10 +0800

    .

--- benchmark/benchmark/remote.py
+++ benchmark/benchmark/remote.py
@@ -358,11 +358,11 @@ class Bench:
             return
 
         # Update nodes.
-        # try:
-        #     self._update(selected_hosts, bench_parameters.collocate)
-        # except (GroupException, ExecutionError) as e:
-        #     e = FabricError(e) if isinstance(e, GroupException) else e
-        #     raise BenchError('Failed to update nodes', e)
+        try:
+            self._update(selected_hosts, bench_parameters.collocate)
+        except (GroupException, ExecutionError) as e:
+            e = FabricError(e) if isinstance(e, GroupException) else e
+            raise BenchError('Failed to update nodes', e)
 
         # Upload all configuration files.
         try:
